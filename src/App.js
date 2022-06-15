33640168c0d6865ea68767a6ec22f8369e6f2c92
Marvin Bonds <marvin.bonds@gmail.com>
2022-06-14 20:37:45 -0500

    lab 2nd iteration

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Hornedbeast from './Hornedbeast';
 import './App.css'
+import './Main.css'
+import './Hornedbeast.css'
 
 class App extends React.Component {
 
@@ -11,9 +14,9 @@ class App extends React.Component {
   render() {
     return (
       <>
-      <Header/>
-      <Main/>
-      <Footer/>
+        <Header />
+        <Main />
+        <Footer />
       </>
     );
   }
--- src/Main.js
+++ src/Main.js
@@ -1,26 +1,25 @@
 import React from "react";
 import Hornedbeast from "./Hornedbeast";
+import data from './data.json'
+import './Main.css'
+
 
 
 class Main extends React.Component {
   render() {
+    let beasts = data.map((beast, index) => {
+        return <Hornedbeast
+        title={beast.title}
+        imageurl={beast.image_url}
+        description={beast.description}
+        key={index}
+        />
+
+  })
+
     return (
       <main>
-        <Hornedbeast
-          title="UniWhal"
-          imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"
-          description="A unicorn and a narwhal nuzzling their horns"
-        />
-        <Hornedbeast
-          title="Rhino Family"
-          imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"
-          description="Parent rhino with two babies"
-        />
-        <Hornedbeast
-          title="Unicorn Head"
-          imageUrl="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"
-          description="Someone wearing a very silly unicorn head mask"
-        />
+        {beasts}
       </main>
     )
   }
